22ef8990eb69695c97bdde1dd10ea2b9485df1e0
JustPlay1994 <764804602@qq.com>
2017-09-25 14:26:27 +0800

    【接口修改】

--- src/main/java/aliyunApi/Update.java
+++ src/main/java/aliyunApi/Update.java
@@ -34,8 +34,8 @@ public class Update {
 
 
         String regionId = "cn-hangzhou"; //必填固定值，必须为“cn-hanghou”
-        String accessKeyId = "LTAImKyenyClI8EV"; // your accessKey
-        String accessKeySecret = "oTMsL9jLrI8ClagbPFSHi9SobjSBxq";// your accessSecret
+        String accessKeyId = "XXXXX"; // your accessKey
+        String accessKeySecret = "XXXXX";// your accessSecret
         IClientProfile profile = DefaultProfile.getProfile(regionId, accessKeyId, accessKeySecret);
         // 若报Can not find endpoint to access异常，请添加以下此行代码
         // DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Alidns", "alidns.aliyuncs.com");
